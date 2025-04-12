911c7dacc126fccd697f2fa506efc6722498cd6e
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 06:39:56 +0300

    (lab.6) task.5

--- lists/list.fs
+++ lists/list.fs
@@ -1,28 +1,57 @@
-let rec reduce_list list (f: int->int->int) (condition: int->bool) acc =
+let rec frequency list num count =
      match list with
-     | [] -> acc
-     | head::tail ->
-         let current = head
-         let newAcc = if condition current then f acc current else acc
-         reduce_list tail f condition newAcc
+     | [] -> count
+     | head :: tail ->
+         let newCount = if head = num then count + 1 else count
+         frequency tail num newCount
+ 
+let rec freq_list list main_list cur_list =
+     match list with
+     | [] -> cur_list
+     | head :: tail ->
+         let freq_elem = frequency main_list head 0
+         let new_list = cur_list @ [freq_elem]
+         freq_list tail main_list new_list
 
-let min_list list = 
+let pos list el =
+     let rec pos_inner list el num =
+         match list with
+         | [] -> 0
+         | head :: tail ->
+             if head = el then num
+             else pos_inner tail el (num + 1)
+     pos_inner list el 1
+
+let get_from_list list pos =
+     let rec get list num cur_num =
+         match list with
+         | [] -> 0
+         | head :: tail ->
+             if num = cur_num then head
+             else get tail num (cur_num + 1)
+     get list pos 1
+ 
+let max_list list =
+     let rec find_max lst current_max =
+         match lst with
+         | [] -> current_max
+         | head :: tail ->
+             let new_max = if head > current_max then head else current_max
+             find_max tail new_max
      match list with
      | [] -> 0
-     | head::tail -> reduce_list list (fun a b -> if a < b then a else b) (fun a -> true) head
+     | head :: tail -> find_max tail head
  
-let sum_even list = reduce_list list (+) (fun a -> a%2 = 0) 0
- 
-let count_odd list = reduce_list list (fun a b -> a+1) (fun a -> a%2 = 1) 0
+let find_most_frequent list =
+     let fL = freq_list list list []
+     let maxFreq = max_list fL
+     let index = pos fL maxFreq
+     get_from_list list index
+
+
 
 let main = 
-    System.Console.Write("Минимум в списке: ")
-    System.Console.WriteLine(min_list [1;2;3;4;5;6])
- 
-    System.Console.Write("Сумма четных в списке: ")
-    System.Console.WriteLine(sum_even [1;2;3;4;5;6])
- 
-    System.Console.Write("Количество нечетных в списке: ")
-    System.Console.WriteLine(count_odd [1;2;3;4;5;6])
+    System.Console.Write("Самый частый элемент: ")
+    System.Console.WriteLine(find_most_frequent [5; 3; 8; 1; 4; 6; 5; 3; 5])
 
 main
