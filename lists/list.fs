3df5588d3f9a6a54ca64b8cab3ed7a6f5ce80350
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 06:53:04 +0300

    (lab.6) task.6

--- lists/list.fs
+++ lists/list.fs
@@ -1,57 +1,31 @@
-let rec frequency list num count =
-     match list with
-     | [] -> count
-     | head :: tail ->
-         let newCount = if head = num then count + 1 else count
-         frequency tail num newCount
+type BinaryTree =
+     | Empty
+     | Node of string * BinaryTree * BinaryTree
  
-let rec freq_list list main_list cur_list =
-     match list with
-     | [] -> cur_list
-     | head :: tail ->
-         let freq_elem = frequency main_list head 0
-         let new_list = cur_list @ [freq_elem]
-         freq_list tail main_list new_list
-
-let pos list el =
-     let rec pos_inner list el num =
-         match list with
-         | [] -> 0
-         | head :: tail ->
-             if head = el then num
-             else pos_inner tail el (num + 1)
-     pos_inner list el 1
-
-let get_from_list list pos =
-     let rec get list num cur_num =
-         match list with
-         | [] -> 0
-         | head :: tail ->
-             if num = cur_num then head
-             else get tail num (cur_num + 1)
-     get list pos 1
+let rec insert tree value =
+     match tree with
+     | Empty -> Node(value, Empty, Empty)
+     | Node(v, left, right) ->
+         if value < v then Node(v, insert left value, right)
+         else Node(v, left, insert right value)
  
-let max_list list =
-     let rec find_max lst current_max =
-         match lst with
-         | [] -> current_max
-         | head :: tail ->
-             let new_max = if head > current_max then head else current_max
-             find_max tail new_max
-     match list with
-     | [] -> 0
-     | head :: tail -> find_max tail head
+let rec inOrder tree =
+     match tree with
+     | Empty -> []
+     | Node(value, left, right) ->
+         inOrder left @ [value] @ inOrder right
  
-let find_most_frequent list =
-     let fL = freq_list list list []
-     let maxFreq = max_list fL
-     let index = pos fL maxFreq
-     get_from_list list index
+let testTree =
+     Empty
+     |> fun t -> insert t "mango"
+     |> fun t -> insert t "apple"
+     |> fun t -> insert t "peach"
+     |> fun t -> insert t "banana"
 
 
 
 let main = 
-    System.Console.Write("Самый частый элемент: ")
-    System.Console.WriteLine(find_most_frequent [5; 3; 8; 1; 4; 6; 5; 3; 5])
+    System.Console.WriteLine("\nСодержимое строки в двоичном дереве (in-order):")
+    inOrder testTree |> List.iter (printfn "%s")
 
 main
