0e2559be17b460ee9963365b90d6d432132ff5d6
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 07:01:51 +0300

    (lab.6) task.7

--- lists/list.fs
+++ lists/list.fs
@@ -1,31 +1,13 @@
-type BinaryTree =
-     | Empty
-     | Node of string * BinaryTree * BinaryTree
- 
-let rec insert tree value =
-     match tree with
-     | Empty -> Node(value, Empty, Empty)
-     | Node(v, left, right) ->
-         if value < v then Node(v, insert left value, right)
-         else Node(v, left, insert right value)
- 
-let rec inOrder tree =
-     match tree with
-     | Empty -> []
-     | Node(value, left, right) ->
-         inOrder left @ [value] @ inOrder right
- 
-let testTree =
-     Empty
-     |> fun t -> insert t "mango"
-     |> fun t -> insert t "apple"
-     |> fun t -> insert t "peach"
-     |> fun t -> insert t "banana"
-
+let find_most_frequent_list_based list =
+     list
+     |> List.countBy id         
+     |> List.maxBy snd          
+     |> fst 
 
 
 let main = 
-    System.Console.WriteLine("\nСодержимое строки в двоичном дереве (in-order):")
-    inOrder testTree |> List.iter (printfn "%s")
+    let test = [5; 3; 8; 1; 4; 6; 5; 3; 5]
+    System.Console.Write("Самый частый элемент: ")
+    System.Console.WriteLine(find_most_frequent_list_based test)
 
 main
