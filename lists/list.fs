5b19bb0902ccd422709f2814623487982abc2e3a
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 06:25:08 +0300

    (lab.6) task.4

--- lists/list.fs
+++ lists/list.fs
@@ -6,9 +6,23 @@ let rec reduce_list list (f: int->int->int) (condition: int->bool) acc =
          let newAcc = if condition current then f acc current else acc
          reduce_list tail f condition newAcc
 
+let min_list list = 
+     match list with
+     | [] -> 0
+     | head::tail -> reduce_list list (fun a b -> if a < b then a else b) (fun a -> true) head
+ 
+let sum_even list = reduce_list list (+) (fun a -> a%2 = 0) 0
+ 
+let count_odd list = reduce_list list (fun a b -> a+1) (fun a -> a%2 = 1) 0
 
 let main = 
-    let sumEven = reduce_list [1;2;3;4;5;6] (+) (fun x -> x % 2 = 0) 0
-    System.Console.WriteLine("Сумма четных: {0}", sumEven)
+    System.Console.Write("Минимум в списке: ")
+    System.Console.WriteLine(min_list [1;2;3;4;5;6])
+ 
+    System.Console.Write("Сумма четных в списке: ")
+    System.Console.WriteLine(sum_even [1;2;3;4;5;6])
+ 
+    System.Console.Write("Количество нечетных в списке: ")
+    System.Console.WriteLine(count_odd [1;2;3;4;5;6])
 
 main
