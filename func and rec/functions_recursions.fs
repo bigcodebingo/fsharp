0ea463733f08904d2d3aa7ab435a780e39d9c700
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 03:18:21 +0300

    (lab.5) task.4

--- func and rec/functions_recursions.fs
+++ func and rec/functions_recursions.fs
@@ -1,29 +1,11 @@
-let circleSquare r =
-    let pi = 3.14159
-    pi * r * r
+let rec sumOfDigits n =
+    if n = 0 then 
+        0
+    else 
+        (n%10) + sumOfDigits (n/10)
 
-let cylindreVolume r h =
-    h * circleSquare r
+printf "введите число:"
+let number = System.Console.ReadLine() |> int
 
-let main() =
-    //суперпозиция
-    printf "введите радиус круга:"
-    let radius = System.Console.ReadLine() |> float
-    printfn "введите высоту цилиндра:"
-    let height = System.Console.ReadLine() |> float
-    let start = circleSquare >> ((*) height)
-    let res = start radius
-
-    printf "объем цилинда (суперпозиция): %f" res
-    
-    //каррирование
-    printf "\nвведите радиус круга:"
-    let radius2 = System.Console.ReadLine() |> float
-    printfn "введите высоту цилиндра:"
-    let height2 = System.Console.ReadLine() |> float
-    let start2 = cylindreVolume radius2
-    let res2 = start2 height2
-
-    printf "объем цилинда (каррирование): %f" res2
-
-main()
+let sum = sumOfDigits number
+printf "сумма цифр %d равна %d" number sum
