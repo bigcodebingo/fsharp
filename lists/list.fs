8a5b6d53b9798b795233a530fd48ef3d0d8fd723
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-12 07:21:35 +0300

    (lab.6) task.9

--- lists/list.fs
+++ lists/list.fs
@@ -1,12 +1,36 @@
-let count_squares list =
+let sum_of_digits n =
+     n
+     |> abs
+     |> string
+     |> Seq.map (fun ch -> int ch - int '0')
+     |> Seq.sum
+ 
+let count_divisors n =
+     let n = abs n
+     [1 .. n] |> List.filter (fun x -> n % x = 0) |> List.length
+ 
+let sort_b list =
      list
-     |> List.filter (fun x -> List.exists (fun y -> y * y = x) list)
-     |> List.length
-
+     |> List.sortBy (fun x -> (sum_of_digits x, -abs x))
+ 
+let sort_c list =
+     list
+     |> List.sortByDescending (fun x -> (count_divisors x, abs x))
+ 
+let make_triples a b c =
+     let sortedA = List.sortDescending a
+     let sortedB = sort_b b
+     let sortedC = sort_c c
+     List.zip3 sortedA sortedB sortedC
 
 let main =  
-    let test = [5; 3; 8; 4; 6; 5; 3; 5; 2]
-    System.Console.Write("Количество квадратов в списке: ")
-    System.Console.WriteLine(count_squares test)
+    let a = [3; 1; 5]
+    let b = [22; 4; 11]  
+    let c = [10; 5; 6]   
+ 
+    let result = make_triples a b c
+ 
+    printfn "Результат:"
+    result |> List.iter (fun (x, y, z) -> printfn "(%d, %d, %d)" x y z)
 
 main
