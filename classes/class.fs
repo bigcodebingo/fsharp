383854fd12baf735d3a5536f8611f4761f6c343b
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-04-19 05:14:35 +0300

    (lab.7) task.3

--- classes/class.fs
+++ classes/class.fs
@@ -1,93 +1,37 @@
-open System
+open FParsec
 
-[<AbstractClass>]
-type GeometricFigure() =
-    abstract member CalculateArea: unit -> float
-    override this.ToString() = 
-        sprintf "Площадь: %.2f" (this.CalculateArea())
+type Expr =
+    | Num of int
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
 
+let numParser: Parser<Expr, unit> =
+    pint32 |>> fun n -> Num n
 
-type IPrint =
-    abstract member Print: unit -> unit
+let addParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '+' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Add (left, right)
 
-type Rectangle(width: float, height: float) =
-    inherit GeometricFigure()
-    member val Width = width with get, set
-    member val Height = height with get, set
-    override this.CalculateArea() =
-        this.Width * this.Height
+let subParser: Parser<Expr, unit> =
+    (spaces >>. numParser .>> spaces) .>>. (pchar '-' .>> spaces) .>>. (spaces >>. numParser)
+    |>> fun ((left, _), right) -> Sub (left, right)
 
-    override this.ToString() =
-        sprintf "Прямоугольник: Ширина = %.2f, Высота = %.2f, Площадь = %.2f" this.Width this.Height (this.CalculateArea())
+let exprParser: Parser<Expr, unit> =
+    addParser <|> subParser <|> numParser
 
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
+let parseExpression (input: string) =
+    match run exprParser input with
+    | Success(result, _, _) -> Some result
+    | Failure(msg, _, _) -> None
 
+let input1 = "3 + 4"
+let input2 = "10 - 3"
+let input3 = "5"
 
-type Square(sideLength: float) =
-    inherit Rectangle(sideLength, sideLength)
-    override this.ToString() =
-        sprintf "Квадрат: Сторона = %.2f, Площадь = %.2f" this.Width (this.CalculateArea())
+let result1 = parseExpression input1
+let result2 = parseExpression input2
+let result3 = parseExpression input3
 
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
-
-
-type Circle(radius: float) =
-    inherit GeometricFigure()
-    member val Radius = radius with get, set
-    override this.CalculateArea() =
-        Math.PI * this.Radius * this.Radius
-
-    override this.ToString() =
-        sprintf "Круг: Радиус = %.2f, Площадь = %.2f" this.Radius (this.CalculateArea())
-
-    interface IPrint with
-        member this.Print() =
-            printfn "%s" (this.ToString())
-
-
-type GeometricFigureType =
-    | Rectangle of width: float * height: float
-    | Square of sideLength: float
-    | Circle of radius: float
-
-
-let calculateArea figure =
-    match figure with
-    | Rectangle (width, height) -> width * height
-    | Square (sideLength) -> sideLength * sideLength
-    | Circle (radius) -> Math.PI * radius * radius
-
-
-let printFigure figure =
-    match figure with
-    | Rectangle (width, height) ->
-        printfn "Прямоугольник: Ширина = %.2f, Высота = %.2f, Площадь = %.2f" width height (calculateArea figure)
-    | Square (sideLength) ->
-        printfn "Квадрат: Сторона = %.2f, Площадь = %.2f" sideLength (calculateArea figure)
-    | Circle (radius) ->
-        printfn "Круг: Радиус = %.2f, Площадь = %.2f" radius (calculateArea figure)
-
-
-let rectangle = Rectangle(5.0, 10.0)
-let square = Square(4.0)
-let circle = Circle(3.0)
-
-//printfn "%s" (rectangle.ToString())
-//printfn "%s" (square.ToString())
-//printfn "%s" (circle.ToString())
-
-//let area1 = rectangle.CalculateArea()
-//let area2 = square.CalculateArea()
-//let area3 = circle.CalculateArea()
-
-printFigure rectangle
-printFigure square
-printFigure circle
-
-//printfn "Площадь прямоугольника: %.2f" (calculateArea rectangle)
-//printfn "Площадь квадрата: %.2f" (calculateArea square)
-//printfn "Площадь круга: %.2f" (calculateArea circle)
+printfn "Результат 1: %A" result1
+printfn "Результат 2: %A" result2
+printfn "Результат 3: %A" result3
